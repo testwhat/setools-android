02a9dcc849fdefc7bc9a97ebadc2fb455aec9e88
R.H <t3stwhat@gmail.com>
2016-07-05 16:18:38 +0800

    Set build env.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,4 @@
-APP_ABI := armeabi armeabi-v7a arm64-v8a mips mips64 x86 x86_64
-APP_PLATFORM := android-16
+#APP_ABI := armeabi armeabi-v7a arm64-v8a mips mips64 x86 x86_64
+#APP_PLATFORM := android-16
+APP_ABI := armeabi-v7a
+APP_PLATFORM := android-19
